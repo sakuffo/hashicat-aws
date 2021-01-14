178d14f254c170bbd5d8f36a936ead7c0e8c4d3e
Stephen Akuffo <sa.akuffo@gmail.com>
2021-01-14 20:53:19 +0000

    test

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -14,7 +14,7 @@ cat << EOM > /var/www/html/index.html
   Welcome to ${PREFIX}'s app. 
   This is the cat of the day. Just look at them!
   Brought by ACME
-  <center><img src="http://photos.app.goo.gl/9wTLh8Lt9Bu8TPvx7"></img></center>
+  <center><img src="https://lh3.googleusercontent.com/pw/ACtC-3c209um-vnjDwRpdEBGlw1CW-t5AtFLTGXGBKeyaIRaL69frrJ9ii5yCn7_288sRGRrPoYAjk6LtIjlbDILRQ955YtACUr5fI0YKIBZfWBEoS3ZDdWuFPY1NTPZP46dgStqgYXh4egx_SFZzZcWPNdv=w1693-h1271-no?authuser=0"></img></center>
   <!-- END -->
 
   </div>
